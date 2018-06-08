0b5e2a4c92f0d6fbe49c735a527e2dbad496c024
felipe <felipe.leite@primeup.com.br>
2018-06-08 18:42:41 +0000

    Update main.go

--- main.go
+++ main.go
@@ -15,37 +15,49 @@ import (
 
 func main() {
 
-	filename := os.Getenv("APP_PATH")
+	filePath := os.Getenv("APP_PATH")
 	appName := os.Getenv("APP_NAME")
 	usrLogin := os.Getenv("USER_LOGIN")
-	senha := os.Getenv("USER_PASSWORD")
+	password := os.Getenv("USER_PASSWORD")
 	presence := os.Getenv("PRESENCE_ID")
 	appUser := os.Getenv("APP_USER")
 	appPassword := os.Getenv("APP_PASSWORD")
-	
-	if len(filename) == 0{
-	    fmt.Println("Filename está vazio.")
-	    os.Exit(10)
+
+	if len(filePath) == 0 {
+		fmt.Println("APK/IPA path is empty")
+		os.Exit(10)
 	}
-	
+	if len(appName) == 0 {
+		fmt.Println("App name is empty")
+		os.Exit(10)
+	}
+	if len(password) == 0 {
+		fmt.Println("AppScan password is empty")
+		os.Exit(10)
+	}
+	if len(usrLogin) == 0 {
+		fmt.Println("AppScan username is empty")
+		os.Exit(10)
+	}
+
 	client := &http.Client{}
-	token, err := login(client, usrLogin, senha)
-	if err != nil || token["Token"] == ""{
+	token, err := login(client, usrLogin, password)
+	if err != nil || token["Token"] == "" {
 		os.Exit(1)
 	}
 
 	idApp, err := findIDApp(client, token, appName)
 	if err != nil {
-		os.Exit(2)
+		//	os.Exit(2)
 	}
 
-	idFile, err := uploadApp(client, token, filename)
+	idFile, err := uploadApp(client, token, filePath)
 	if err != nil {
-		os.Exit(4)
+		//	os.Exit(4)
 	}
 	_, err = doScanMobile(client, appName, token, idFile, idApp, appUser, appPassword, presence)
 	if err != nil {
-		os.Exit(5)
+		//	os.Exit(5)
 	}
 
 	fmt.Println("Terminating the application...")
@@ -75,12 +87,18 @@ func login(client *http.Client, usrLogin string, senha string) (map[string]strin
 	data, _ := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(data, &m)
 
+	if m["Token"] == "" {
+		fmt.Println("Not able to log in. Check your username and password.")
+		return nil, errors.New("Not able to log in. Check your username and password")
+	}
+
+	fmt.Println("Exiting login...")
 	return m, nil
 }
 
-func findIDApp(client *http.Client, token map[string]string, nomeApp string) (string, error) {
+func findIDApp(client *http.Client, token map[string]string, nameApp string) (string, error) {
 	fmt.Println("Starting getting apps...")
-	var retorno []map[string]interface{}
+	var apps []map[string]interface{}
 
 	req, err := http.NewRequest("GET", "https://appscan.ibmcloud.com/api/V2/Apps", nil)
 	if err != nil {
@@ -96,30 +114,30 @@ func findIDApp(client *http.Client, token map[string]string, nomeApp string) (st
 	}
 
 	data, _ := ioutil.ReadAll(resp.Body)
-	json.Unmarshal(data, &retorno)
-	for _, app := range retorno {
-		if app["Name"] == nomeApp {
-			fmt.Println("ID do App ", app["Id"])
+	json.Unmarshal(data, &apps)
+	for _, app := range apps {
+		if app["Name"] == nameApp {
+			fmt.Println("App Id: ", app["Id"])
 			id, _ := app["Id"].(string)
 			return id, nil
 		}
 	}
-	fmt.Println("No application found with the name " + nomeApp + " in AppScan")
-	return "", errors.New("No application found with the name " + nomeApp + " in AppScan")
+	fmt.Println("No application found with the name " + nameApp + " in AppScan")
+	return "", errors.New("No application found with the name " + nameApp + " in AppScan")
 }
-func uploadApp(client *http.Client, token map[string]string, filename string) (string, error) {
+func uploadApp(client *http.Client, token map[string]string, filePath string) (string, error) {
 	fmt.Println("Starting to upload files...")
 
 	bodyBuffer := &bytes.Buffer{}
 	bodyWriter := multipart.NewWriter(bodyBuffer)
 
-	fileWriter, err := bodyWriter.CreateFormFile("fileToUpload", filename)
+	fileWriter, err := bodyWriter.CreateFormFile("fileToUpload", filePath)
 	if err != nil {
 		fmt.Println("Error writting the request's body: ", err)
 		return "", err
 	}
 
-	fileHandle, err := os.Open(filename)
+	fileHandle, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("Error opening the APK/IPA file: ", err)
 		return "", err
@@ -141,10 +159,10 @@ func uploadApp(client *http.Client, token map[string]string, filename string) (s
 	}
 
 	// index do primeiro caracter do boundary
-	inicioBoundary := strings.Index(bodyWriter.FormDataContentType(), "=") + 1
+	startBoundary := strings.Index(bodyWriter.FormDataContentType(), "=") + 1
 
 	//substring que forma o boundary
-	boundary := bodyWriter.FormDataContentType()[inicioBoundary:]
+	boundary := bodyWriter.FormDataContentType()[startBoundary:]
 
 	req.Header.Add("Content-Type", "multipart/form-data; boundary=\""+boundary+"\"")
 	req.Header.Add("Accept-Encoding", "gzip,deflate")
@@ -158,21 +176,27 @@ func uploadApp(client *http.Client, token map[string]string, filename string) (s
 		return "", err
 	}
 
+	if resp.StatusCode != 201 {
+		fmt.Printf("The HTTP request failed with status %d\n", resp.StatusCode)
+		return "", errors.New("The HTTP request failed with status " + string(resp.StatusCode))
+	}
+
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Printf("Error reading the response data %s\n", err)
 		return "", err
 	}
-	resp.Body.Close()
-	retorno := make(map[string]string)
-	json.Unmarshal(data, &retorno)
 
-	fmt.Println("Exiting upload...")
-	return retorno["FileId"], nil
+	resp.Body.Close()
+	responseData := make(map[string]string)
+	json.Unmarshal(data, &responseData)
+
+	fmt.Println("Upload Succesful...")
+	return responseData["FileId"], nil
 }
 
 func doScanMobile(client *http.Client, name string, token map[string]string, idFile string, idApp string, login string, senha string, presence string) (map[string]string, error) {
-	fmt.Println("Starting scan......")
+	fmt.Println("Starting scan...")
 
 	m := make(map[string]string)
 
@@ -204,6 +228,11 @@ func doScanMobile(client *http.Client, name string, token map[string]string, idF
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 		return nil, err
+	}
+
+	if resp.StatusCode != 201 {
+		fmt.Printf("The HTTP request failed with status %d\n", resp.StatusCode)
+		return nil, errors.New("The HTTP request failed with status " + string(resp.StatusCode))
 	}
 
 	data, _ := ioutil.ReadAll(resp.Body)
